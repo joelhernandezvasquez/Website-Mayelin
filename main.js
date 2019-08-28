126401a33122e890ed213c3dc901209a1cdb5539
Joel Hernandez <jhernandez98@outlook.es>
2019-08-28 18:52:59 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,91 @@
+
+//GLOBAL VARIABLES FOR APPOINTMENT Form
+
+function hoverMenuItems(e)
+{
+  if(e.target.closest("li"))
+  {
+    link= e.target.firstElementChild;
+    link.style.color = "white";
+   
+  }
+}
+
+function hoverOutMenuItems(e)
+{
+  if(e.target.closest("li"))
+ {
+   link= e.target.firstElementChild;
+   link.style.color = "#e75aa1";
+  
+ }
+}
+
+function hoverLinks(e)
+{
+  if(e.target.closest("a"))
+ {
+   e.target.style.color = "white";
+  
+ }
+}
+
+function openMenuBar(e)
+{
+  menuBar.classList.toggle("open-menu-bar");
+  menuIcon.classList.toggle("change"); // animating the icon menu
+}
+
+function openAppointmentForm(e)
+{
+ const appointmentForm = document.querySelector(".appointment-container");
+ appointmentForm.classList.add("openAppointmentForm");
+
+}
+
+function closeAppointmentForm()
+{
+  const appointmentForm = document.querySelector(".appointment-container");
+  const fullName = document.querySelector("#name");
+  const email = document.querySelector("#email");
+  
+  fullName.value="";
+  email.value ="";
+  appointmentForm.classList.remove("openAppointmentForm");
+
+}
+
+function submitAppointment(e)
+{
+
+ const fullName = document.querySelector("#name");
+ const email = document.querySelector("#email");
+ const nameErrorMessage = document.querySelector("#nameError");
+ const emailErrorMessage = document.querySelector("#emailError");
+  const name = fullName.value.trim();
+  const emailInput = email.value.trim();
+
+ if(!name)
+ {
+   nameErrorMessage.style.display ="block";
+   fullName.focus();
+   e.preventDefault();
+  
+ }
+ else
+ {
+  nameErrorMessage.style.display ="none";
+ }
+
+ if(!emailInput)
+ {
+  emailErrorMessage.style.display ="block";
+  email.focus();
+  e.preventDefault();
+ }
+ else
+ {
+  emailErrorMessage.style.display ="none";
+ }
+ 
+}
